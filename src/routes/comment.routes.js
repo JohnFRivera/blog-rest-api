30ce84102714a4813ff9430f82d218aee4984fe0
JohnFRivera <johnfrivera1309@gmail.com>
2024-09-14 13:32:58 -0500

    Reparación del codigo de las rutas y los controladores de Comment 2

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { getQuoteComments, createComment, deleteComment } from "../controllers/comment.controllers.js";
+import { getAllCommentsByQuotePk, createComment, deleteComment } from "../controllers/comment.controllers.js";
 
 const router = Router();
 /**
  * Public
  */
-router.get("/comment/quote/:id", getQuoteComments); //? Traer comentario por el id de la cita
+router.get("/comment/quote/:id", getAllCommentsByQuotePk); //? Traer comentario por el id de la cita
 router.post("/comment/create", createComment); //? Crear comentario
 router.delete("/comment/delete/:id", deleteComment); //? eliminar comentario
 
